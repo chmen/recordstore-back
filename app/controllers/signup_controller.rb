3224f309781ec43bf1a68965d0258d19b479ceb6
chmen <tony.potapov@gmail.com>
2019-07-04 17:28:38 +0300

    update signup

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -2,7 +2,7 @@ class SignupController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      payload = { user_id: user.id }
+      payload  = { user_id: user.id }
       session = JWTSessions::Session.new(payload: payload, refresh_by_access_allowed: true)
       tokens = session.login
 
@@ -10,13 +10,14 @@ class SignupController < ApplicationController
                           value: tokens[:access],
                           httponly: true,
                           secure: Rails.env.production?)
-      render json: {csrf: tokens[:csrf]}
+      render json: { csrf: tokens[:csrf] }
     else
-      render json: { error: user.errors.full_messages.join(' ')}, status: :unprocessable_entity
+      render json: { error: user.errors.full_messages.join(' ') }, status: :unprocessable_entity
     end
   end
 
   private
+
   def user_params
     params.permit(:email, :password, :password_confirmation)
   end
