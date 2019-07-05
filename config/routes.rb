3ab65b2021c762134408bad995c5a3be62c5092f
chmen <tony.potapov@gmail.com>
2019-07-05 10:10:11 +0300

    update routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,14 @@ Rails.application.routes.draw do
   get 'signin/destory'
   namespace :api do
     namespace :v1 do
-      resources :records
-      resources :artists
+      resources :artists do
+        resources :records
+      end
     end
   end
 end
+
+post 'refresh', controller: :refresh, action: :create
+post 'signin', controller: :signin, action: :create
+post 'signup', controller: :signup, action: :create
+delete 'signin', controller: :signin, action: destroy
