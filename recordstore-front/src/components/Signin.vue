88a99671bb1fc25a590a6caa0df7ef9b5bc03c4c
chmen <tony.potapov@gmail.com>
2019-07-19 09:47:28 +0300

    fix intendetion in signin

--- recordstore-front/src/components/Signin.vue
+++ recordstore-front/src/components/Signin.vue
@@ -22,47 +22,47 @@
 </template>
 
 <script>
-  export default {
-    name: 'Signin',
-    data () {
-      return {
-        email: '',
-        password: '',
-        error: ''
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      email: '',
+      password: '',
+      error: ''
+    }
+  },
+  created () {
+    this.checkSignedIn()
+  },
+  updated () {
+    this.checkSignedIn()
+  },
+  methods: {
+    signin () {
+      this.$http.plain.post('/signin', { email: this.email, password: this.password })
+        .then(response => this.signinSuccessful(response))
+        .catch(error => this.signinFailed(error))
+    },
+    signinSuccessful (response) {
+      if (!response.data.csrf) {
+        this.signinFailed(response)
+        return
       }
+      localStorage.csrf = response.data.csrf
+      localStorage.signedIn = true
+      this.error = ''
+      this.$router.replace('/records')
     },
-    created () {
-      this.checkSignedIn()
+    signinFailed (error) {
+      this.error = (error.response && error.response.data && error.response.data.error) || ''
+      delete localStorage.csrf
+      delete localStorage.signedIn
     },
-    updated () {
-      this.checkSignedIn()
-    },
-    methods: {
-      signin () {
-        this.$http.plain.post('/signin', { email: this.email, password: this.password })
-          .then(response => this.signinSuccessful(response))
-          .catch(error => this.signinFailed(error))
-      },
-      signinSuccessful (response) {
-        if (!response.data.csrf) {
-          this.signinFailed(response)
-          return
-        }
-        localStorage.csrf = response.data.csrf
-        localStorage.signedIn = true
-        this.error = ''
+    checkSignedIn () {
+      if (localStorage.signedIn) {
         this.$router.replace('/records')
-      },
-      signinFailed (error) {
-        this.error = (error.response && error.response.data && error.response.data.error) || ''
-        delete localStorage.csrf
-        delete localStorage.signedIn
-      },
-      checkSignedIn () {
-        if (localStorage.signedIn) {
-          this.$router.replace('/records')
-        }
       }
     }
   }
+}
 </script>
